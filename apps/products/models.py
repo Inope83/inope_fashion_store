341a07c9d5974b9daff72ecf39ce6a8fb05bea88
inope83 <angelinorosaleslopes1234@gmail.com>
2026-06-12 23:04:47 +0900

    hamo nia homepage

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Kategoria(models.Model):
+    naran = models.CharField(max_length=255)
+    deskrisaun = models.TextField(blank=True)
+
+    @classmethod
+    def list_all(cls):
+        return cls.objects.all()
+
+    def __str__(self):
+        return self.naran
+
+    class Meta:
+        verbose_name_plural = "Kategorias"
+
+
+class Produtu(models.Model):
+    naran = models.CharField(max_length=255)
+    deskrisaun = models.TextField(blank=True)
+    preco = models.FloatField()
+    stok = models.IntegerField(default=0)
+    imagem = models.ImageField(upload_to='produtu/', blank=True, null=True)
+    kategoria = models.ForeignKey(
+        Kategoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='produtu_set'
+    )
+
+    def check_stock(self):
+        return self.stok > 0
+
+    def __str__(self):
+        return self.naran
+
+    class Meta:
+        verbose_name_plural = "Produtus"
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
-
-from django.urls import include, path
+from django.contrib import admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-] 
+    path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+]
