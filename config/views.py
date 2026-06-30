d2d65e8af66dcd62259a196c879d0ad9e6508deb
inope83 <angelinorosaleslopes1234@gmail.com>
2026-06-30 23:00:29 +0900

    fix: admin user iha / redirect ba dashboard, la'o customer homepage

--- config/views.py
+++ config/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from apps.products.models import Produtu
+from apps.users.models import Kliente
 
 
 def home_view(request):
+    kliente_id = request.session.get('kliente_id')
+    if kliente_id and Kliente.objects.filter(id=kliente_id, is_staff=True).exists():
+        return redirect('dashboard:dashboard_home')
+    if request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser):
+        return redirect('dashboard:dashboard_home')
     return render(request, 'home/home.html', {
         'featured': Produtu.objects.prefetch_related('images').all()[:8],
     })
